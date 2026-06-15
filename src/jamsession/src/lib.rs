a8827e4f03f118ae7622e1eb70ed56131ec4ed54
Niko Matsakis <niko@alum.mit.edu>
2026-06-15 20:21:00 +0000

    chore: add some deny

--- src/jamsession/src/lib.rs
+++ src/jamsession/src/lib.rs
@@ -1,3 +1,6 @@
+#![deny(unused_must_use)] // Must-use warnings are almost always bugs. Propagate results or document why you do not have to.
+#![deny(unsafe_code)] // Unsafe code does not belond in this crate. Avoid it or, if truly needed, create a carefully thought out abstraction.
+
 pub mod agent;
 pub mod daemon;
 mod dispatcher;
